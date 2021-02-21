8c3f4fdbd07576ecf61d80ba7dfc8e515b83fb6a
Dario Campagna <campagnacoutinho67@gmail.com>
2021-02-21 15:03:01 -0300

    Criação da classe aluno.

---
+++ src/com/java/modulo01/Aluno.java
@@ -0,0 +1,27 @@
+package com.java.modulo01;
+
+import java.util.Objects;
+
+public class Aluno {
+
+    private String nome;
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Aluno outro = (Aluno) obj;
+        return outro.getNome().equals(this.nome);
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
