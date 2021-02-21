89fdec3799edd0e6c9dd7bc86cb907c225e83dc7
Dario Campagna <campagnacoutinho67@gmail.com>
2021-02-21 15:31:09 -0300

    Criação dos métodos.

--- src/com/java/modulo01/Vetor.java
+++ src/com/java/modulo01/Vetor.java
@@ -1,4 +1,28 @@
 package com.java.modulo01;
 
+import java.util.Arrays;
+
 public class Vetor {
+
+    private Aluno [] alunos = new Aluno[100];
+
+    public void adiciona(Aluno aluno){
+
+    }
+
+    public void remove(int posicao){
+
+    }
+
+    public boolean contem(Aluno aluno){
+        return false;
+    }
+
+    public int tamanho(){
+        return 0;
+    }
+
+    public String toString(){
+        return Arrays.toString(alunos);
+    }
 }
