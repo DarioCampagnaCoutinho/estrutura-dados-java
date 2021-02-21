1b448b679948d9e0ffd3702fe1d1f590f899ce1c
Dario Campagna <campagnacoutinho67@gmail.com>
2021-02-21 15:38:58 -0300

    Criação do método de principal.

--- src/com/java/Main.java
+++
@@ -1,8 +0,0 @@
-package com.java;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("Olá Java");
-    }
-}
---
+++ src/com/java/modulo01/Main.java
@@ -0,0 +1,22 @@
+package com.java.modulo01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("Dario");
+        Aluno a2 = new Aluno("Fario");
+        Aluno a3 = new Aluno("Mario");
+        Aluno a4 = new Aluno("Yario");
+        Aluno a5 = new Aluno("Jario");
+        Aluno a6 = new Aluno("Pario");
+
+        Vetor lista = new Vetor();
+
+        lista.adiciona(a1);
+        lista.adiciona(a2);
+        lista.adiciona(a3);
+
+        System.out.println(lista);
+    }
+}
